6666b09a9ce37795993d47484c21912ed3f7e41d
minbyunghyuk <ptsdkr@naver.com>
2020-04-20 16:44:21 +0900

    initcommit springboot jpa

---
+++ src/main/java/com/bitacademy/datadust/DatadustApplication.java
@@ -0,0 +1,13 @@
+package com.bitacademy.datadust;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatadustApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DatadustApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/bitacademy/datadust/ssg/entity/SearchItemRepository.java
@@ -0,0 +1,10 @@
+package com.bitacademy.datadust.ssg.entity;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface SearchItemRepository  extends JpaRepository<SeachItemEntity,Long> {
+
+
+}
